7f82c87463b09324f8bc32b52102f84ed42ec97d
Ben Butler <benbutleranimator@gmail.com>
2024-06-27 15:47:30 +0000

    Add review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
+
+RATING = ((0, "0 stars"), (1, "1 star"), (1, "2 stars"), (1, "3 stars"), (1, "4 stars"), (1, "5 stars"))
 
 class Movie(models.Model):
     title = models.CharField(max_length=255, blank=True)
@@ -9,3 +12,19 @@ class Movie(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../clapper_board_vector_oi5zxv'
     )
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="reviews")
+    movie_title = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, related_name="reviews")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField(blank=True)
+    rating = models.IntegerField(default=3, choices=RATING)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.movie_title}'
