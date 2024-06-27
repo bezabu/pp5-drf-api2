23e5ede343f404fcd7d1a05cb0e1b702fd42a242
Ben Butler <benbutleranimator@gmail.com>
2024-06-27 16:16:51 +0000

    Add movies serializer and view

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Movie
+from .serializers import MovieSerializer
 
+
+class MovieList(APIView):
+    """
+    List all movies
+    No Create view (post method), as profile creation handled by django signals
+    """
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
 # Create your views here.
