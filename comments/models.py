d0ce78ddf67ff414b8d54a023727a5dcd9e89de5
Ben Butler <benbutleranimator@gmail.com>
2024-06-28 15:05:00 +0000

    Add comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Review
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Review
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
