e0a3aa27370c7fb844f2e4a4cda31260bb1cbc8c
Ben Butler <benbutleranimator@gmail.com>
2024-06-28 21:13:16 +0000

    Add like and follow ids to reviews and profiles

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from reviews.models import Movie, Review
+from reviews.models import Review
+from likes.models import Like
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -9,6 +10,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     movie_image = serializers.ReadOnlyField(source='movie.image.url')
 
@@ -16,10 +18,19 @@ class ReviewSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, review=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Review
         fields = [
             'id', 'owner', 'movie', 'movie_title', 'movie_image', 'created_at',
-            'updated_at', 'content', 'rating', 'is_owner', 'profile_id', 'profile_image'
+            'updated_at', 'content', 'rating', 'is_owner', 'profile_id', 'profile_image', 'like_id',
         ]
 
