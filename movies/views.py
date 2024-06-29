741ccbd95ede08791a97430eeb80e0caaf8156c6
Ben Butler <benbutleranimator@gmail.com>
2024-06-29 16:13:44 +0000

    revert movie add permissions

--- movies/views.py
+++ movies/views.py
@@ -38,8 +38,7 @@ class MovieCreate(generics.CreateAPIView):
     Only accessible by movie curators
     """
     serializer_class = MovieSerializer
-    #permission_classes = [permissions.IsAuthenticated, IsCuratorOrReadOnly]
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsCuratorOrReadOnly]
 
 class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
     """
