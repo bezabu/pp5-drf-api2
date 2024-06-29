453c570c3bfcfa154ecd0f3b9adc340c59a74cfc
Ben Butler <benbutleranimator@gmail.com>
2024-06-29 13:27:03 +0000

    Install djr auth

--- pp5_drf_api2/settings.py
+++ pp5_drf_api2/settings.py
@@ -54,6 +54,13 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken', 
+    'dj_rest_auth',
+    'django.contrib.sites', 
+    'allauth', 
+    'allauth.account', 
+    'allauth.socialaccount', 
+    'dj_rest_auth.registration',
     'profiles',
     'movies',
     'reviews',
@@ -61,7 +68,7 @@ INSTALLED_APPS = [
     'likes',
     'follows',
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
