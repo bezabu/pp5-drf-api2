d444af6b0b5137f76d73cd7860867d88ba78aacd
Ben Butler <benbutleranimator@gmail.com>
2024-07-04 20:40:23 +0000

    prep for combination

--- pp5_drf_api2/settings.py
+++ pp5_drf_api2/settings.py
@@ -81,10 +81,10 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [( 
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-        #'rest_framework.authentication.SessionAuthentication' 
-        #if 'DEV' in os.environ 
-        #else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        #'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        'rest_framework.authentication.SessionAuthentication' 
+        if 'DEV' in os.environ 
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
