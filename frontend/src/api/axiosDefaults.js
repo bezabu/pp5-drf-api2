eb0614cf7fb088589d729560edf54e4c567abc72
Ben Butler <benbutleranimator@gmail.com>
2024-07-04 20:49:52 +0000

    tweak env vars

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://bb-pp5-movie-review-app-363d95a342e4.herokuapp.com/';
+//axios.defaults.baseURL = 'https://bb-pp5-movie-review-app-363d95a342e4.herokuapp.com/';
 //axios.defaults.baseURL = 'https://bb-pp5-api-c8624a6ee9af.herokuapp.com/';
+
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
