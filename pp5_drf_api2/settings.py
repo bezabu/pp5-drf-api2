755aad7bf58980fda17c36e12bc4f7ed54b216e8
Ben Butler <benbutleranimator@gmail.com>
2024-07-04 21:32:10 +0000

    static files

--- pp5_drf_api2/settings.py
+++ pp5_drf_api2/settings.py
@@ -212,7 +212,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+#STATIC_URL = str(BASE_DIR / 'static/')
+STATIC_URL = '/static/'
 WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 
 # Default primary key field type
