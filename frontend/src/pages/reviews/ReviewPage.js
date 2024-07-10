442a1a645b533fa736653a9bd0b7425ec831e60e
Ben Butler <benbutleranimator@gmail.com>
2024-07-10 09:30:12 +0000

    Add comments to reviews

--- frontend/src/pages/reviews/ReviewPage.js
+++ frontend/src/pages/reviews/ReviewPage.js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import appStyles from "../../App.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useParams } from "react-router-dom/cjs/react-router-dom";
@@ -13,16 +14,19 @@ import Review from "./Review";
 function ReviewPage() {
   const { id } = useParams();
   const [ review, setReview ] = useState({results: []});
-
+    const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] }); 
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{data: review}] = await Promise.all([
-          axiosReq.get(`/reviews/${id}`)
+        const [{data: review}, {data: comments}] = await Promise.all([
+          axiosReq.get(`/reviews/${id}`),
+          axiosReq.get(`/comments/?review=${id}`)
         ])
         setReview({results: [review]})
-        console.log(review)
+        setComments(comments)
       } catch(err){
         console.log(err)
       }
@@ -38,7 +42,26 @@ function ReviewPage() {
         <p>Popular profiles for mobile</p>
         <Review {...review.results[0]} setReviews={setReview} reviewPage />
         <Container className={appStyles.Content}>
-          Comments
+          {currentUser ? (
+          <CommentCreateForm
+          profile_id={currentUser.profile_id}
+          profileImage={profile_image}
+          review={id}
+          setReview={setReview}
+          setComments={setComments}
+          />
+          ) : comments.results.length ? (
+          "Comments"
+          ) : null}
+          {comments.results.length ? (
+            comments.results.map(comment => (
+              <p key={comment.id}>{comment.owner} - {comment.content}</p>
+            ))
+          ) : currentUser ? (
+            <span>Nothing here</span>
+          ) : (
+            <span>Nothin here yet</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
