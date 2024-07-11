e37c9290f3e56bca14fe8a0175b742e208475d2b
Ben Butler <benbutleranimator@gmail.com>
2024-07-11 20:59:56 +0000

    fix settings.py

--- pp5_drf_api2/settings.py
+++ pp5_drf_api2/settings.py
@@ -114,7 +114,6 @@ REST_AUTH_SERIALIZERS = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
