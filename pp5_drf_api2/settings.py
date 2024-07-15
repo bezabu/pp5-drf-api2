5e7407e6248d70d8da0062ab606028db83a0703c
Ben Butler <benbutleranimator@gmail.com>
2024-07-15 10:16:44 +0000

    Put back trailing /, heroku error

--- pp5_drf_api2/settings.py
+++ pp5_drf_api2/settings.py
@@ -218,7 +218,7 @@ USE_TZ = True
 
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 #STATIC_URL = str(BASE_DIR / 'static/')
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 
 # Default primary key field type
