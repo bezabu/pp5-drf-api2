295f275298724d19848cb25d4cc999bd90344f87
Ben Butler <benbutleranimator@gmail.com>
2024-07-15 10:45:32 +0000

    recompile for heroku

--- pp5_drf_api2/settings.py
+++ pp5_drf_api2/settings.py
@@ -114,6 +114,7 @@ REST_AUTH_SERIALIZERS = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -216,9 +217,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 #STATIC_URL = str(BASE_DIR / 'static/')
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = BASE_DIR / 'static/'
 WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 
 # Default primary key field type
